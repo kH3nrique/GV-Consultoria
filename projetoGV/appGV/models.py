11ee534fa30a6e93934b98ce1d15b4021637055c
kH3nrique <kh3nriqueafs18@gmail.com>
2024-01-26 17:42:55 -0300

    main

--- projetoGV/appGV/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    id_user = models.AutoField(primary_key=True)
-    nome = models.TextField(max_length=255)
-    idade = models.IntegerField()
--- projetoGV/projetoGV/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from appGV import views
-
-urlpatterns = [
-    #rota, view, nome de referência 
-    path('', views.home, name='home'), 
-
-    path('usuarios/', views.users, name='listagemUsuarios')
-]
